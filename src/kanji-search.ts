05074599333b4bb51f01c0d5f8022f45647a703c
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2022-03-19 11:55:33 +0100

    set the tabId in local storage

--- src/kanji-search.ts
+++ src/kanji-search.ts
@@ -1,4 +1,5 @@
 const SEARCH_KANJI = 'search-kanji'
+const TAB_ID_KEY = 'search-kanji-tab-id'
 
 chrome.contextMenus.create(
   {
@@ -8,21 +9,23 @@ chrome.contextMenus.create(
   }
 )
 
+const getItem = (key: string) => new Promise(res => chrome.storage.local.get([key], (result) => res(result?.[key])));
+const setItem = (key: string, value: string) => new Promise(res => chrome.storage.local.set({ [key]: value }, () => res(value)));
+
 const tabExists = (tabId: number) => chrome.tabs.get(tabId)
   .then((val) => !!val)
   .catch(() => false)
 
-let kanjiSearchTab: chrome.tabs.Tab;
-
 const handleKanjiSearch = async (info: chrome.contextMenus.OnClickData) => {
-  const text = info.selectionText?.trim()
+  const text = info.selectionText?.trim()[0]
   const url = `https://www.japandict.com/kanji/${text}`
 
-  const tabId = kanjiSearchTab?.id
-  if (tabId && await tabExists(tabId)) {
+  const tabId = Number(await getItem(TAB_ID_KEY))
+  if (tabId > 0 && await tabExists(tabId)) {
     chrome.tabs.update(tabId, { active: true, url })
   } else {
-    kanjiSearchTab = await chrome.tabs.create({ url })
+    const tab = await chrome.tabs.create({ url })
+    await setItem(TAB_ID_KEY, tab?.id?.toString() || '')
   }
 }
 
